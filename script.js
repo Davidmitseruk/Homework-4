1d6245d13619e776b52da03b9bd957dac4bd55aa
Davidmitseruk <davidmitseruk@gmail.com>
2025-02-24 15:45:41 +0100

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+// import * as test from './test.js';
+// console.log(test.hello());
+// console.log(test.bye());
+
+
+import { square, sqrt2 } from './test.js' ;
+console.log(square(11));
+console.log(square(5, 7));
---
+++ test.js
@@ -0,0 +1,9 @@
+//  export const helloMessage = 'hello!';
+// export const Byemessage = 'Bye!';
+
+// export const hello = () => helloMessage;
+// export const bye = () => Byemessage;
+
+const sqrt = Math.sqrt;
+export const square = x => x * x;
+export const sqrt2 = (x, y) => sqrt(square(x) + square(y));
